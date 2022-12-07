aff161f292eebcbff15dd7cfd4177a5256b5c611
Hanhui Song <hsong42@ucsc.edu>
2022-12-06 23:48:47 -0600

    cool

--- server/config/secrets.js
+++ server/config/secrets.js
@@ -3,6 +3,6 @@
  */
 module.exports = {
     token : "secret-starter-mern",
-    mongo_connection : "mongodb+srv://hsong42:1234567Project@project.lhv54oi.mongodb.net/?retryWrites=true&w=majority"
+    mongo_connection : "mongodb+srv://hsong42:1234567Project@project.lhv54oi.mongodb.net/test?retryWrites=true&w=majority"
     //example: mongo_connection : "mongodb+srv://[type-yours]:[type-yours]@[type-yours-web-provided].mongodb.net/test?retryWrites=true"
 };
