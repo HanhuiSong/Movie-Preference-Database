51eb757ba0f63520878724f53012edac88fa3ac7
XuiucWil <xinyang8@illinois.edu>
2022-12-11 16:25:35 -0600

    gallery card & header

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -34,7 +34,7 @@ const Header = () => {
             <AppBar position="static" color="primary" style={{ background: '#202020' }}>
                 <Toolbar>
 
-                    <Typography variant="h6" align = "center" component="div" sx={{ flexGrow: 1}}>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1}}>
                         <Button href={"/"}>Interactive Movie DB</Button>
                     </Typography>
 
